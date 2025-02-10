5aa582877bce61c1203c5c564dd0d129ee229b24
NilimaSardar <snilima181@gmail.com>
2025-02-10 19:53:12 +0545

    menu active css is done

--- Hunger/src/components/ExploreMenu/ExploreMenu.jsx
+++ Hunger/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { menu_list } from '../../assets/frontend_assets/assets'
 import styled from 'styled-components'
 
-const ExploreMenu = () => {
+const ExploreMenu = ({category, setCategory}) => {
   return (
     <ExploreMenuStyle id='explore-menu'>
       <h1>Explore our menu</h1>
@@ -15,8 +15,8 @@ const ExploreMenu = () => {
       <div className="explore-menu-list">
         {menu_list.map((item,index)=>{
           return(
-            <div key={index} className="explore-menu-list-item">
-              <img src={item.menu_image} alt="" />
+            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
+              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
               <p>{item.menu_name}</p>
             </div>
           )
@@ -64,7 +64,7 @@ const ExploreMenuStyle = styled.div`
     min-width: 80px;
     cursor: pointer;
     border-radius: 50%;
-    transition: 0ms.2s;
+    transition: 0.2s;
   }
 
   .explore-menu-list-item p{
@@ -79,5 +79,10 @@ const ExploreMenuStyle = styled.div`
     height: 2px;
     background-color: #e2e2e2;
     border: none;
+  }
+
+  .explore-menu-list-item .active{
+    border: 4px solid tomato;
+    padding: 2px;
   }
 `;
