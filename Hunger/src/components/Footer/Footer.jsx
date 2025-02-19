02f66615791739d3b3b3c333af486913d622028c
NilimaSardar <snilima181@gmail.com>
2025-02-19 16:02:47 +0545

    added footer content

--- Hunger/src/components/Footer/Footer.jsx
+++ Hunger/src/components/Footer/Footer.jsx
@@ -1,9 +1,47 @@
 import React from 'react'
+import styled from 'styled-components'
+import { assets } from '../../assets/frontend_assets/assets'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <FooterStyle id='footer'>
+      <div className="footer-content">
+        <div className="footer-content-left">
+          <img src={assets.logo} alt="" />
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maiores nesciunt et amet laboriosam accusamus quisquam. Temporibus ut quam debitis doloribus!</p>
+          <div className="footer-social-icons">
+            <img src={assets.facebook_icon} alt="" />
+            <img src={assets.twitter_icon} alt="" />
+            <img src={assets.linkedin_icon} alt="" />
+          </div>
+        </div>
+        <div className="footer-content-center">
+          <h2>COMPANY</h2>
+          <ul>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        <div className="footer-content-right">
+          <h2>GET IN TOUCH</h2>
+          <ul>
+            <li>+977 9824637481</li>
+            <li>contact@tomato.com</li>
+          </ul>
+        </div>
+      </div>
+      <hr />
+      <p className="footer-copyright">
+        Copyright 2024 Tomato.com - All Right Reserved.
+      </p>
+    </FooterStyle>
   )
 }
 
 export default Footer
+
+const FooterStyle = styled.div`
+
+`;
